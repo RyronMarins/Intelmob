a6846dc24cad3bb8a26d940f84e6b080228d4a85
RyronMarins <ryronmarins@gmail.com>
2023-08-26 19:16:01 +0000

    reset

--- templates/teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Verifica se a função crypt está disponível
-if (function_exists('crypt')) {
-    echo "A função crypt está disponível no servidor.";
-} else {
-    echo "A função crypt não está disponível no servidor.";
-}
-?>
